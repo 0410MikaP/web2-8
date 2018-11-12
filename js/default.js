07089f1eee2c69383d9987cd6616da00f09ce0a6
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-12 19:05:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,9 +1,10 @@
-var HP=100;
-while(0<HP){
-  var li=document.createElement('li');
-  var damage =Math.floor(Math.random()*100);
-  HP-=damage;
-  HP=(HP<0)?0:HP;
-  li.textContent=damage+'のダメージを受けた。残りのHP:'+HP;
+var include_tax=function(price){
+  return Math.floor(price+0.08*price);
+}
+
+for(var i=1;i<10;i++){
+  var li=document.createElemrnt('li');
+  var price=Math.floor(Math.random()*1000);
+  li.textContent=price+'円（税込み'+include_tax(price)+'円')';
   document.getElementById('list').appendChild(li);
 }
