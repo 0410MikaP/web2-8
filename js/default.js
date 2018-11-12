646ea675d7e8440cb8872076d2f414e65cb7c01d
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-12 19:18:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-var constellatio=['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];var
+var constellatio=['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];
 for(var i=0;i<12;i++){
   var li=document.createElement('li');
   li.textContent=constellatio[i];
