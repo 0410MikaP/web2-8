6e9e3b87dd68bd02a6d0d1e13c970f8a69e882b0
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-12 18:55:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,9 @@
-for(var i =1; i<10;i++){
+var HP=100;
+while(0<HP){
   var li=document.createElement('li');
-  li.textContent=i+'回目';
+  var damage =Math.floor(Math.random()*100);
+  HP-=damage;
+  HP=(HP<0)?0:HP;
+  li.textContent=damage+'のダメージを受けた。残りのHP:'+HP;
   document.getElementById('list').appendChild(li);
 }
