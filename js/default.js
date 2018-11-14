683a5153734bc0e89b15a63a08cec9967448f504
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-14 16:14:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,12 +6,15 @@ var separate_time=function(time){
   return[sec,min,hours,days];
 }
 
-var now=new Date();
-var target = new Date(2020,7,21,0,0,0,0);
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent =
-  counter[3]+'日'+
-  counter[2]+'時'+
-  counter[1]+'分'+
-  counter[0]+'秒';
+var update = function(){
+  var now = new Date();
+  var target = new Date(2020,7,25,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_Time(diff)+
+  document.getElementById('countdown').textContent =
+    '誕生日まであと'+
+    counter[3]+'日'+
+    counter[2]+'時'+
+    counter[1]+'分'+
+    counter[0]+'秒';
+refresh();//タイマーを起動
