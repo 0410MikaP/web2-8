f5e9ee4b2d78b7462f43aa24a2aa0a13f1855d27
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-14 15:10:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
-var constellatio=['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];
-for(var i=0;i<12;i++){
-  var li=document.createElement('li');
-  li.textContent=constellatio[i];
+var player={name:'みか',age:20,blood:'A'};
+for(key in player){
+  var li =document.createElement('li');
+  li.textContent=key+':'+player[key];
   document.getElementById('list').appendChild(li);
 }
