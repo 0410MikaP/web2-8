5ba36dbda43eea678fd9bde6c94ff35454e58618
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-19 16:51:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,8 @@
 var separate_time=function(time){
-  var sec = Math.floor((time/1000)%60);
-  var min = Math.floor((time/1000/60)%60);
-  var hours = Math.floor((time/1000/60/60)%24);
-  var days = Math.floor(time/1000/60/60/24);
+  var sec = time.getSeconds();
+  var min = time.getMinutes();
+  var hours = time.getHours();
+  var days = time.getDate();
   return[sec,min,hours,days];
 }
 
@@ -21,7 +21,6 @@ refresh();//タイマーを起動
 }
 
 var refresh= function(){
-  setTimeout(update,1000);
-  
+  setTimeout(update,1000); 
 }
 update();
